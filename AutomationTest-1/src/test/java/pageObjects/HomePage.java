82df941d1157a99cb16535b60af877ad3097cf94
Saritha Nair <saritha.nair@thermofisher.com>
2021-06-04 22:13:13 +0530

    First commit

---
+++ AutomationTest-1/src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,33 @@
+package pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+public class HomePage extends UIBasePage{
+
+	public HomePage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	public Select select;
+
+	@FindBy(xpath = "//div[@class='container']//form/select[@name='fromPort']")
+	WebElement departureSelectDropdown;
+
+	@FindBy(xpath = "//div[@class='container']//form/select[@name='toPort']")
+	WebElement destinationSelectDropdown;
+
+	@FindBy(xpath = "//div[@class='container']/input[@type='submit']")
+	WebElement submitButton;
+
+	public void bookflight(String departure, String destination) {
+		select = new Select(departureSelectDropdown);		
+		select.selectByVisibleText(departure);
+		select = new Select(destinationSelectDropdown);
+		select.selectByVisibleText(destination);
+		submitButton.click();
+	}
+
+}
